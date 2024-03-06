38f8a4c3b8b3e100c17048ae2fe57d6dea41cff0
Jakub <31663@uniwersytetkaliski.edu.pl>
2024-03-06 04:00:10 +0100

    1

---
+++ app/druga_zagadka/page.tsx
@@ -0,0 +1,9 @@
+import Test from "@/app/ui/background";
+
+export default function Home() {
+    return (
+        <div className=".bg-no-repeat ">
+            bla
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import Test from "@/app/ui/background"
+import Link from "next/link";
 
 export default function Home() {
   return (
-          <div className=".bg-no-repeat ">
-              <Test/>
-              <div
-                  className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-                  <h1 className="text-3xl font-bold mb-4">Welcome to my Next.js App</h1>
-                  <div className="mx-auto" style={{width: '300px', height: '200px'}}>
-                      <Image
-                          src="/images/example.jpg"
-                          alt="Example Image"
-                          layout="responsive"
-                          width={300}
-                          height={200}
-                      />
-                  </div>
+      <div className=".bg-no-repeat ">
+          <Test/>
+          <div
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+              <div className="bg-gray-800 bg-opacity-50 px-6 py-4 rounded-lg">
+                  <h1 className="text-3xl font-bold mb-4">
+                      Kochany Piotrusiu po ostatniej zagadke pewnie wiesz, że trzeba zwracać uwage
+                      na rzeczy których
+                      <Link href="/druga_zagadka">
+                          <h1 className="underline text-yellow-400 hover:text-yellow-500
+                           transition duration-300 ease-in-out pulsate">nie widać gołym okiem</h1>
+                      </Link>{" "}
+                      reszta tekstu
+                  </h1>
               </div>
           </div>
+      </div>
   );
 }
