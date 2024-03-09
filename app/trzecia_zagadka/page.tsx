d8901b80d1ebcb0ccd715298cdb73940b63fa6f7
Jakub969473 <124714550+Jakub969473@users.noreply.github.com>
2024-03-09 03:39:37 +0100

    Update page.tsx

--- app/trzecia_zagadka/page.tsx
+++ app/trzecia_zagadka/page.tsx
@@ -12,9 +12,7 @@ export default function Home() {
             <div
                 className="bg-gray-800 bg-opacity-50 px-6 py-4 rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
                 <div>
-                    <h1>Teścik</h1>
-                    <h2>Musisz mieć 100% aby przejść do następnego zadania</h2>
-                    <TicTacToe/>
+                    <h1>Poproś tomeczka o kodzik <3</h1
                 </div>
             </div>
         </div>
